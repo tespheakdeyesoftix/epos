1da6de4f2f78c51b7576b3dee9ad11849b0fb49c
rathana-bit <76212180+Rathana-bit@users.noreply.github.com>
2025-05-19 17:33:23 +0700

    update

--- src/views/sales/SaleDetail.vue
+++ src/views/sales/SaleDetail.vue
@@ -3,60 +3,58 @@
         <ToolBar>{{ t("Sale Detail") }}</ToolBar>
         <ion-content class="ion-padding">
             <ion-card button="true" class="mx-0">
-                <ion-card-content>
-                    <ion-grid>
-                        <ion-row>
-                            <ion-col size="auto">
-                                <ion-chip class="m-0">
-                                    <ion-avatar>
-                                        <img :alt="doc?.customer_name" src="/assets/avatar.svg" />
-                                    </ion-avatar>
-                                    <ion-label>{{ doc?.customer_name }}</ion-label>
-                                </ion-chip>
-                                <ion-card-subtitle>{{ doc?.customer }} / {{ doc?.phone_number }}</ion-card-subtitle>
-                            </ion-col>
-                            <ion-col class="text-right">
-                                <ion-button shape="round" size="small" style="--background:#533B4D">
-                                    <ion-icon slot="icon-only" :icon="searchOutline"></ion-icon>
-                                </ion-button>
-                                <ion-button shape="round" size="small" style="--background:#7F55B1">
-                                    <ion-icon slot="icon-only" :icon="personAddOutline"></ion-icon>
-                                </ion-button>
-                                <ion-button shape="round" size="small" style="--background:#328E6E">
-                                    <ion-icon slot="icon-only" :icon="scanCircleOutline"></ion-icon>
-                                </ion-button>
-                            </ion-col>
-                        </ion-row>
-                    </ion-grid>
-
-                </ion-card-content>
-            </ion-card>
-
-            <template v-for="(d, index) in doc?.sale_products" :key="index">
-                <ion-card class="ion-no-margin mb-2">
-                    <ion-item lines="none">
-                        <ion-avatar>
-                            <Img v-if="d?.photo" :src="d?.photo" />
-                            <div class="avatar-placeholder" v-else>{{ getAvatarLetter(d.product_name) }}</div>
+                <ion-card-content class="py-3 px-0">
+                    <ion-item>
+                        <ion-avatar slot="start">
+                        <img :alt="doc?.customer_name" src="/assets/avatar.svg" />
                         </ion-avatar>
                         <ion-label>
-                            <h2 class="card-title">{{ d.product_code }} - {{ d.product_name }}</h2>
-                            <h2 class="card-subtitle">{{d.quantity}} x <com-currency :value="d.price"/></h2>
-                            <h2 v-if="d.modifiers && !d.is_timer_product" class="card-subtitle">{{d.modifiers}} (<com-currency :value="d.modifiers_price * d.quantity"/>)</h2>
-                             
-                            <h2 class="card-subtitle flex align-items-center gap-1" v-if="d.time_in || d.time_out">
-                                <ion-icon size="small" :icon="timeOutline"></ion-icon> {{ dayjs(d.time_in).format('hh:mm A') }} {{d?.time_out?'-':''}} {{ dayjs(d.time_out).format('hh:mm A') }}
-                            </h2> 
-                            <h2 class="card-subtitle flex align-items-center gap-1" v-else>
-                                <ion-icon size="small" :icon="timeOutline" ></ion-icon> {{ dayjs(d.creation).format('hh:mm:ss A') }}
-                            </h2>
+                            <div>{{ doc?.customer_name }}</div>
+                            <div>{{ doc?.customer }} {{doc?.phone_number?'/':''}} {{ doc?.phone_number }}</div>
                         </ion-label>
-                        <ion-label slot="end" class="amount-container flex flex-column align-items-center">
-                            <div class="amount"><com-currency :value="d?.price" /></div>
-                            <ion-chip color="primary">{{ d.quantity }}</ion-chip>
-                        </ion-label>
-                    </ion-item> 
-                </ion-card>
+                    </ion-item>
+                </ion-card-content>
+            </ion-card>
+            <!-- group product -->
+            <template v-if="getSaleProducts.length > 0">
+                <template v-for="(g, index) in getSaleProductGroupByKey" :key="index">
+                    <div class="bg-red-700 text-white flex items-center justify-between" style="font-size: 10px; padding: 2px;">
+                        <div>
+                            <ion-icon size="small" :icon="timeOutline" ></ion-icon>
+                            {{dayjs(g.order_time).format('HH:mm:ss')}}
+                        </div>
+                        <div><v-icon icon="mdi-account-outline" size="small" class="mr-1"></v-icon>{{ g.order_by }}</div>
+                    </div>
+                    <!-- product section -->
+                    <div>
+                        <template v-for="(d, index) in doc?.sale_products" :key="index">
+                            <ion-card class="ion-no-margin mb-2">
+                                <ion-item lines="none">
+                                    <ion-avatar>
+                                        <Img v-if="d?.photo" :src="d?.photo" />
+                                        <div class="avatar-placeholder" v-else>{{ getAvatarLetter(d.product_name) }}</div>
+                                    </ion-avatar>
+                                    <ion-label>
+                                        <h2 class="card-title">{{ d.product_code }} - {{ d.product_name }}</h2>
+                                        <h2 class="card-subtitle">{{d.quantity}} x <com-currency :value="d.price"/></h2>
+                                        <h2 v-if="d.modifiers && !d.is_timer_product" class="card-subtitle">{{d.modifiers}} (<com-currency :value="d.modifiers_price * d.quantity"/>)</h2>
+                                        
+                                        <h2 class="card-subtitle flex align-items-center gap-1" v-if="d.time_in || d.time_out">
+                                            <ion-icon size="small" :icon="timeOutline"></ion-icon> {{ dayjs(d.time_in).format('hh:mm A') }} {{d?.time_out?'-':''}} {{ dayjs(d.time_out).format('hh:mm A') }}
+                                        </h2> 
+                                        <h2 class="card-subtitle flex align-items-center gap-1" v-else>
+                                            <ion-icon size="small" :icon="timeOutline" ></ion-icon> {{ dayjs(d.creation).format('hh:mm:ss A') }}
+                                        </h2>
+                                    </ion-label>
+                                    <ion-label slot="end" class="amount-container flex flex-column align-items-center">
+                                        <div class="amount"><com-currency :value="d?.price" /></div>
+                                        <ion-chip color="primary">{{ d.quantity }}</ion-chip>
+                                    </ion-label>
+                                </ion-item> 
+                            </ion-card>
+                        </template>
+                    </div>
+                </template>
             </template>
         </ion-content>
     </ion-page>
@@ -65,9 +63,10 @@
 </template>
 <script setup>
 import { dice, searchOutline, personAddOutline, scanCircleOutline, timeOutline } from 'ionicons/icons';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import { getAvatarLetter } from "@/helpers/utils"
 import dayjs from 'dayjs';
+import Enumerable from 'linq';
 
 
 const t = window.t;
@@ -83,9 +82,27 @@ async function loadData() {
 
 }
 onMounted(async () => {
+    alert(1)
     await loadData()
 })
 
+const getSaleProducts = computed(() => { 
+    alert(2)
+    if (!doc.value?.sale_products) return []
+    return Enumerable.from(doc?.value.sale_products).orderByDescending("$.modified").toArray()
+})
+
+const getSaleProductGroupByKey = computed(() => {
+    if (!doc?.value.sale_products) {
+        return []
+    } else {
+        if (doc?.value.sale_products) {
+            const group = Enumerable.from(doc?.value.sale_products).groupBy("{order_by:$.order_by,order_time:$.order_time}", "", "{order_by:$.order_by,order_time:$.order_time}", "$.order_by+','+$.order_time");
+            return group.orderByDescending("$.order_time").toArray();
+        }
+        return []
+    }
+})
 </script>
 <style scoped>
 .card-title {
