11650526c122daa598e6b41c1afa2af9024d6a48
DOMY <outdom.leap@esoftix.com>
2025-05-20 04:56:29 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,40 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/', 
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5 MB limit
+      },
+      manifest: {
+        name: 'ePOS',
+        short_name: 'ePOS',
+        description: 'ePOS for Owner',
+        theme_color: '#3880ff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
     legacy()
   ],
   resolve: {
