4d1bff9a871fa1a180583e7adbc15a8c8671c102
Sengho825 <limsabriey94@gmail.com>
2025-05-24 09:50:44 +0700

    update

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -15,19 +15,24 @@
                 }}</ion-button>
             </ion-item>
         </ComSelect>
+       
             <stack class="ion-padding">
                 <ComDashboardKPI :data="ctrl.kpiData" />
                 <ComDashboardChart :data="ctrl.chartData" />
                 <ComRecentOrder :data="ctrl.recentData" />
+                <ComPaymentBreakDown/>
             </stack>
+             
         </ion-content>
     </ion-page>
+    
 </template>
 <script setup >
 
 import ComRecentOrder from "@/views/dashboard/components/ComRecentOrder.vue"
 import ComDashboardChart from "@/views/dashboard/components/ComDashboardChart.vue"
 import ComDashboardKPI from "@/views/dashboard/components/ComDashboardKPI.vue"
+import ComPaymentBreakDown from "@/views/dashboard/components/ComPaymentBreakDown.vue"
 import { storefrontOutline } from 'ionicons/icons';
 
 import { useDashboard } from "@/hooks/useDashboard.js"
