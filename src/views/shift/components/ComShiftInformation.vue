a0d8d41b56ca4c8c7bc45363a234644cda6ff802
lakleabheng <lakleabheng@gmail.com>
2025-06-14 11:44:12 +0700

    update

--- src/views/shift/components/ComShiftInformation.vue
+++ src/views/shift/components/ComShiftInformation.vue
@@ -1,5 +1,5 @@
 <template>
-    <ion-card>
+    <ion-card v-if="data">
   <ion-card-header>
     <ion-card-title> {{ t('Shift Information') }}</ion-card-title>
   </ion-card-header>
@@ -8,29 +8,29 @@
       <ion-row>
         <ion-col size="6">
           <strong>{{t('Working Day')}}:</strong><br />
-          <ion-text color="medium">{{shift?.working_day}}</ion-text>
+          <ion-text color="medium">{{data?.working_day}}</ion-text>
         </ion-col>
         <ion-col size="6">
           <strong>{{ t('Cashier Shift') }} :</strong><br />
-          <ion-text color="medium">{{ shift?.name }}</ion-text>
+          <ion-text color="medium">{{ data?.name }}</ion-text>
         </ion-col>
       </ion-row>
 
       <ion-row>
         <ion-col size="6">
           <strong>{{t('Close Date')}}:</strong><br />
-          <ion-text color="medium">{{shift?.posting_date}}</ion-text>
+          <ion-text color="medium">{{data?.posting_date}}</ion-text>
         </ion-col>
         <ion-col size="6">
           <strong>{{$t('Shift Name')}}:</strong><br />
-          <ion-text color="medium">{{shift?.shift_name}}</ion-text>
+          <ion-text color="medium">{{data?.shift_name}}</ion-text>
         </ion-col>
       </ion-row>
 
       <ion-row>
         <ion-col size="12">
-          <strong>POS Profile:</strong><br />
-          <ion-text color="medium">{{shift?.pos_profile}}</ion-text>
+          <strong>{{$t('POS Profile')}} :</strong><br />
+          <ion-text color="medium">{{data?.pos_profile}}</ion-text>
         </ion-col>
       </ion-row>
     </ion-grid>
@@ -39,11 +39,7 @@
 </template>
 <script setup>
 const t = window.t;
-import { onMounted, ref, computed } from 'vue';
-const shift = ref()
-onMounted(async () => {
- shift.value = (await app.postApi("epos_restaurant_2023.api.api.get_current_cashier_shift",{
-        pos_profile: "Main POS Profile"
- })).data
-});
+const props = defineProps({
+    data:Object
+})
 </script>
