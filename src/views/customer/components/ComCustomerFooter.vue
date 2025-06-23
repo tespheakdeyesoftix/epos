d741a3f42ae6875fde1a6088991b03d77ea7ad56
Sabrey <limsabriey94@gmail.com>
2025-06-23 17:29:12 +0700

    update

---
+++ src/views/customer/components/ComCustomerFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <ion-footer>
+        
+        <ion-toolbar  >
+            <div style="display: flex; justify-content: center;gap: 10px;"> 
+            <ion-button routerLink="/customer"> {{ t('Close') }}</ion-button>
+            
+            
+            <ComQuickPayButton/>
+            <ion-button color="success" @click="onPaymentClick"> {{ t('Edit') }}</ion-button>
+            </div>
+           
+        </ion-toolbar>
+    </ion-footer>
+</template>
+<script setup>
+const t = window.t;
+</script>
