e33808f30d652cbb7e369bd46beccc688e877d0d
Sabrey <limsabriey94@gmail.com>
2025-06-28 17:30:07 +0700

    update

--- src/modules/ecoupon/dashboard/components/ComCouponBreakdownChart.vue
+++ src/modules/ecoupon/dashboard/components/ComCouponBreakdownChart.vue
@@ -33,11 +33,11 @@
             <ion-content class="ion-padding">
               <ion-list>
                 <ion-item button @click="onChangeChartType('bar')">
-                  <!-- <ion-icon slot="start" :icon="barChartOutline"></ion-icon> -->
+                  <ion-icon slot="start" :icon="barChartOutline"></ion-icon>
                   <ion-label>{{ t("View Graph as Bar Chart") }}</ion-label>
                 </ion-item>
                 <ion-item button @click="onChangeChartType('line')">
-                  <!-- <ion-icon slot="start" :icon="analyticsOutline"></ion-icon> -->
+                  <ion-icon slot="start" :icon="analyticsOutline"></ion-icon>
                   <ion-label>{{ t("View Graph as Line Chart") }}</ion-label>
                 </ion-item>
                 <ion-item button @click="onViewData">
