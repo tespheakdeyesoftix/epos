ac38b193ca7babe892a75a7eeea9650c64d05054
Sabrey <limsabriey94@gmail.com>
2025-07-03 16:51:45 +0700

    update

---
+++ src/views/working-day/WorkingDayList.vue
@@ -0,0 +1,57 @@
+<template>
+    <ion-page>
+        <AppBar>{{ t("Working Day List") }}</AppBar>
+        <ion-content >
+
+          
+            <DocList docType="Working Day" :options="options"
+            @onRowDblClick="onRowDblClick"
+            >
+              <template #is_closed="{item}">
+                <ComStatus status="Opened" color="danger" v-if="item.is_closed == 0" />
+                <ComStatus status="Closed" v-else />
+              </template>
+            </DocList>
+        </ion-content>
+    </ion-page>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const plateform = ref(app.utils.getPlateform())
+ 
+const t = window.t
+const options = {
+    columns:[
+        {fieldname:"name",header:"Working Day #",url:'/working-day-detail' },
+        {fieldname:"posting_date",header:"Date" ,fieldtype:"Date"},
+        {fieldname:"pos_profile",header:"POS Profile"},
+        {fieldname:"outlet",header:"Outlet"},
+        {fieldname:"created_by",header:"Created By"},
+        {fieldname:"closed_date",header:"Closed Date" ,fieldtype:"Date"},
+        {fieldname:"closed_by",header:"Closed By"},
+        {fieldname:"is_closed",header:"Status"}, 
+    ],
+    showSearchBar:true,
+    showBarcodeScanner:false,
+    fields: ["name"],
+    orderBy:{
+      field: "modified",
+      order: "desc",
+  },
+  filters:[
+    ["business_branch","=",app.setting.property.property_name]
+],
+
+  filterOptions:[
+    {fieldname:"posting_date", fieldtype:"Date", label:t('Sale Date'),clear:true,modal_type:plateform=='mobile'?'sheet_modal':'modal'},
+    {fieldname:"pos_profile", fieldtype:"Link",options:"POS Profile", label:t('POS Profile')},
+    {fieldname:"outlet", fieldtype:"Link",options:"Outlet", label:t('Outlet')},
+     
+  ]
+}
+
+function onRowDblClick(data){
+    app.ionRouter.navigate("/working-day-detail/" + data.name, "forward", "push");
+}
+</script>
