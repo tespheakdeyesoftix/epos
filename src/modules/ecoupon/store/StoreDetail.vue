765f4d1e6243d77bf6e43d006115b65e73255bea
Sabrey <limsabriey94@gmail.com>
2025-07-16 17:51:14 +0700

    update

--- src/modules/ecoupon/store/StoreDetail.vue
+++ src/modules/ecoupon/store/StoreDetail.vue
@@ -1,11 +1,27 @@
 <template>
     <ion-page>
-        <ToolBar>{{ t("Store Detail") }}</ToolBar>
+        <ToolBar>{{ t("Store Detail") }} - {{ data?.name || 0}}</ToolBar>
         <ion-content>
             stordetail
+            {{ data }}
         </ion-content>
     </ion-page>
 </template>
 <script setup>
+import { ref, onMounted } from 'vue'
 const t = window.t
+
+const data = ref(null)
+async function loadData() {
+  const l = await app.showLoading()
+
+  const res = await app.getDoc("Vendor", app.route.params.name)
+  if (res.data) data.value = res.data
+
+  await l.dismiss()
+}
+
+onMounted(async () => {
+  await loadData()
+})
 </script>
