a4f79fce714bf3bd7c8cbdaf2b158fbe13547756
Sabrey <limsabriey94@gmail.com>
2025-07-19 15:28:12 +0700

    update

--- src/views/sales/components/ComTopCouponInforDesktop.vue
+++ src/views/sales/components/ComTopCouponInforDesktop.vue
@@ -12,44 +12,49 @@
    
        
 </Column>
-
-<Column headerClass="text-center" bodyClass="text-center" :header="t('Remaining Value')">
-  <template #body="slotProps">    
-    {{ slotProps.data.coupons?.[0]?.balance_coupon_value}}
-  </template>
-</Column>
   <Column headerClass="text-center" bodyClass="text-center" :header="t('Remaining Amount')">
   <template #body="slotProps">    
     {{ slotProps.data.coupons?.[0]?.balance_amount }}
   </template>
 </Column>
-
-
-
-   <Column field="price" headerClass="text-right" bodyClass="text-right" :header="t('Top Up Value')">
-      <template #body="slotProps">  
-        <ComCurrency :value="slotProps.data.price"/>
-      </template>
+<Column headerClass="text-center" bodyClass="text-center" :header="t('Remaining Value')">
+  <template #body="slotProps">    
+    {{ slotProps.data.coupons?.[0]?.balance_coupon_value}}
+  </template>
 </Column>
+
+
   <Column field="amount" headerClass="text-right" bodyClass="text-right" :header="t('Top Up Amount')">
       <template #body="slotProps">  
-        <ComCurrency :value="slotProps.data.price"/>
+        <ComCurrency :value="slotProps.data.amount"/>
       </template>
 </Column>
-<Column field="total_coupon_value" headerClass="text-right" bodyClass="text-right" :header="t('Total Value')">
+
+   <Column field="total_coupon_value" headerClass="text-right" bodyClass="text-right" :header="t('Top Up Value')">
+      <template #body="slotProps">  
+        <ComCurrency :value="slotProps.data.total_coupon_value"/>
+      </template>
+</Column>
+
+<Column  headerClass="text-right" bodyClass="text-right" :header="t('Total Value')">
       <template #body="slotProps">
-        <ComCurrency :value="Math.abs(slotProps.data.total_coupon_value)" />
+        <ComCurrency 
+        :value="Math.abs((slotProps.data.coupons?.[0]?.balance_amount || 0) + (slotProps.data.amount || 0))" 
+        />
       </template>
 </Column>
 
-
-  <Column headerClass="text-right" bodyClass="text-right" :header="t('Total Amount')">
+ <Column headerClass="text-right" bodyClass="text-right" :header="t('Total Amount')">
   <template #body="slotProps">
     <ComCurrency 
       :value="Math.abs((slotProps.data.coupons?.[0]?.balance_coupon_value || 0) + (slotProps.data.total_coupon_value || 0))" 
     />
   </template>
 </Column>
+
+
+
+ 
    
 </DataTable>
 </template> 
