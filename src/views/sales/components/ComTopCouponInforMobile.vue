1c583459dd903aa8dd65c9f78cbd02b7d8a270eb
Sabrey <limsabriey94@gmail.com>
2025-07-21 09:19:50 +0700

    update

--- src/views/sales/components/ComTopCouponInforMobile.vue
+++ src/views/sales/components/ComTopCouponInforMobile.vue
@@ -1,25 +1,79 @@
 <template>
  
-{{ saleProducts }}
+<!-- {{ saleProducts }} -->
     <template v-for="(d, index) in saleProducts" :key="index">
-            <ion-card class="ion-no-margin mb-2">
-                 
-                         
-                                <ion-item>
-                                    
-                                    <ion-avatar>
+            <ion-card class="ion-no-margin ion-no-padding">
+                                <ion-item lines="none">
+                                    <!-- <ion-avatar>
                                         <Img v-if="d?.product_photo" :src="d?.product_photo" />
                                         <div class="avatar-placeholder" v-else>{{ getAvatarLetter(d.product_name) }}
                                         </div>
-                                    </ion-avatar>
-                                    <ion-label style="margin-left: 10px;">
-                                        <h2 class="card-title">{{t("Coupon Code")}} - {{ d.product_code }}</h2>
-                                        <div class="card-title">{{t("Coupon Name")}} - {{ d.product_name }}</div>
-                                        <h2 class="card-title">{{t("Top Up Amount") }} - <com-currency :value="d.amount"/></h2>
-                                        <div class="card-title">{{t("Top Up Value")}} - {{ d.total_coupon_value }}</div>
-                                        <h2 class="card-title">{{t("Top Up Amount") }} - <com-currency :value="d.amount"/></h2>
-                                        <div class="card-title">{{t("Top Up Value")}} - {{ d.total_coupon_value }}</div>
-                                        <p class="card-subtitle" style="color: black;"><com-currency :value="d.price"/></p>
+                                    </ion-avatar> -->
+                                    <ion-label class="ion-no-padding ">
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                                {{t("Coupon Code")}} 
+                                            </ion-label>
+                                            <ion-label slot="end">
+                                                {{ d.product_code }}
+                                            </ion-label>
+                                        </ion-item>
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                                {{t("Coupon Name")}}
+                                            </ion-label>
+                                             <ion-label slot="end">
+                                                {{ d.product_name }}
+                                            </ion-label>
+                                        </ion-item>
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                                {{t("Remaining Amount") }}
+                                            </ion-label>
+                                            <ion-label slot="end">
+                                                <ComCurrency :value="d.coupons?.[0]?.balance_amount" />
+                                            </ion-label>
+                                        </ion-item>
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                                {{t("Remaining Value")}} 
+                                            </ion-label>
+                                            <ion-label slot="end">
+                                                <ComCurrency :value="d.coupons?.[0]?.balance_coupon_value" />
+                                            </ion-label>
+                                        </ion-item>
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                                {{t("Top Up Amount") }}
+                                            </ion-label>
+                                            <ion-label slot="end">
+                                                <ComCurrency :value="d.amount"/>
+                                            </ion-label>
+                                        </ion-item>
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                                {{t("Top Up Value")}} 
+                                            </ion-label>
+                                            <ion-label slot="end">
+                                                <ComCurrency :value="d.total_coupon_value"/>
+                                            </ion-label>
+                                        </ion-item>
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                                {{t("Total Amount")}} 
+                                            </ion-label>
+                                            <ion-label slot="end">
+                                                <ComCurrency :value="Math.abs((d.coupons?.[0]?.balance_coupon_value || 0) + (d.total_coupon_value || 0))" />
+                                            </ion-label>
+                                        </ion-item>
+                                        <ion-item class="card-title">
+                                            <ion-label>
+                                            {{t("Total Value")}}
+                                            </ion-label>
+                                            <ion-label slot="end">
+                                            <ComCurrency :value="Math.abs((d.coupons?.[0]?.balance_amount || 0) + (d.amount || 0))" />
+                                            </ion-label>
+                                        </ion-item>       
                                     </ion-label>
                                     
                                      
