afcdaef8068a4a188275f0112be9e55368640f7a
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2025-07-30 15:52:33 +0700

    update

--- src/modules/ecoupon/CouponRegister/CouponRegister.vue
+++ src/modules/ecoupon/CouponRegister/CouponRegister.vue
@@ -1,30 +1,23 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>{{ t('Coupon Register') }} - {{ doc?.name }}</ion-title>
-        <ion-chip color="success" @click="onSave" slot="end" class="mr-5">  
+ 
+    <ToolBar>
+{{ t('Coupon Register') }} - {{ doc?.name }}
+<template #end >
+ <ion-chip color="success" @click="onSave" slot="end" class="mr-5">
           <ion-label>{{ t("Save") }}</ion-label>
         </ion-chip>
-      </ion-toolbar>
-    </ion-header>
+</template>
+    </ToolBar>
 
     <ion-content>
       {{ doc }}
       <div class="fix-container">
         <ion-card style="margin-top: 30px;">
           <ion-card-content>
-            <com-input
-              ref="inputRef"
-              focus
-              v-model="coupon"
-              @change="onScanBarCode"
-              :label="t('Coupon Code')"
-              :placeholder="t('Please enter or scan qr code')"
-              label-placement="stacked"
-              fill="outline"
-            />
-            
+            <com-input ref="inputRef" focus v-model="coupon" @change="onScanBarCode" :label="t('Coupon Code')"
+              :placeholder="t('Please enter or scan qr code')" label-placement="stacked" fill="outline" />
+
             <div style="display: flex; justify-content: center; margin-top: 10px;">
               <ion-chip color="success" @click="onChangeScanMode('add')">
                 <ion-icon v-if="scanMode == 'add'" :icon="checkmarkOutline"></ion-icon>
@@ -39,28 +32,23 @@
         </ion-card>
 
         <div class="ion-padding" v-if="doc?.name">
-          <DocList 
-            ref="docListRef"
-            docType="Coupon Codes"
-            :options="options"
-            :focus="false"
-          >
-        <template #coupon_status="{item}">
-        
-          <template v-if="item.coupon_status=='Saving...'">
-          <ion-spinner name="dots"></ion-spinner>
-                  {{ item.coupon_status }}
+          <DocList ref="docListRef" docType="Coupon Codes" :options="options" :focus="false">
+            <template #coupon_status="{ item }">
+
+              <template v-if="item.coupon_status == 'Saving...'">
+                <ion-spinner name="dots"></ion-spinner>
+                {{ item.coupon_status }}
+              </template>
+              <template v-else-if="item.coupon_status == 'Done'">
+                <ion-icon :icon="checkmarkCircle" color="success" size="large" />
+                {{ t("Done") }}
+              </template>
+              <ComStatus :status="item.coupon_status" v-else />
+
+              <ion-label v-if="item.message" color="danger">{{ item.message }}</ion-label>
+
             </template>
-          <template v-else-if ="item.coupon_status=='Done'">
-                <ion-icon :icon="checkmarkCircle" color="success" size="large"/>
-                  {{ t("Done") }}
-            </template>
-          <ComStatus :status="item.coupon_status" v-else />
-          
-<ion-label v-if="item.message" color="danger" >{{ item.message  }}</ion-label>
-          
-        </template>
-        </DocList>
+          </DocList>
         </div>
       </div>
     </ion-content>
@@ -69,33 +57,33 @@
 
 
 <script setup>
- 
+
 import { ref, onMounted } from 'vue';
 
 import dayjs from "dayjs";
 import { checkboxOutline, checkmarkCircle, checkmarkOutline, closeOutline } from 'ionicons/icons';
-import {handleErrorMessage} from "@/helpers/error-message.ts"
+import { handleErrorMessage } from "@/helpers/error-message.ts"
 const doc = ref(null);
 import PQueue from 'p-queue'
 
 const queue = new PQueue({ concurrency: 1 })
 
 const options = {
-    columns:[
-        {fieldname:"coupon",header:"Coupon #",url:"/coupon-detail",id_field:"name"},
-        {fieldname:"coupon_status",header:"Status",},
-     
- 
-    ],
-    showSearchBar:true,
-    showBarcodeScanner:false,
-    fields: ["name"],
-    orderBy:{
-      field: "modified",
-      order: "desc",
+  columns: [
+    { fieldname: "coupon", header: "Coupon #", url: "/coupon-detail", id_field: "name" },
+    { fieldname: "coupon_status", header: "Status", },
+
+
+  ],
+  showSearchBar: true,
+  showBarcodeScanner: false,
+  fields: ["name"],
+  orderBy: {
+    field: "modified",
+    order: "desc",
   },
 
-  
+
 }
 
 const docListRef = ref(null)
@@ -108,74 +96,74 @@ const inputRef = ref(null)
 const coupon = ref("")
 
 function onChangeScanMode(mode) {
-    scanMode.value = mode;
-    inputRef.value.focus()
+  scanMode.value = mode;
+  inputRef.value.focus()
 }
 
 async function onScanBarCode() {
-    if (scanMode.value == "add") {
-        await addCoupon()
-    } else {
-        onRemoveCoupon()
-    }
+  if (scanMode.value == "add") {
+    await addCoupon()
+  } else {
+    onRemoveCoupon()
+  }
 }
 function onDelete(index) {
-    coupounList.value.splice(index, 1)
-    inputRef.value.select()
-    coupon.value = "";
+  coupounList.value.splice(index, 1)
+  inputRef.value.select()
+  coupon.value = "";
 }
 async function addCoupon() {
-  
+ 
 
   if (coupon.value == "") {
-        app.showWarning("Plese enter or scan coupon code")
-    } 
-
-      const coupon_data = {
-      id:app.utils.generateRandomString(),
-      coupon_register:doc.value.name,
-      coupon:coupon.value,
-      coupon_status:"Saving..."
+    app.showWarning("Plese enter or scan coupon code")
   }
-   docListRef.value.addRecord(coupon_data)
 
-    queue.add(() => addCouponQueue(JSON.parse(JSON.stringify( coupon_data))))
-     coupon.value = ""
-    inputRef.value.focus()
+  const coupon_data = {
+    id: app.utils.generateRandomString(),
+    coupon_register: doc.value.name,
+    coupon: coupon.value,
+    coupon_status: "Saving..."
+  }
+  docListRef.value.addRecord(coupon_data)
+
+  queue.add(() => addCouponQueue(JSON.parse(JSON.stringify(coupon_data))))
+  coupon.value = ""
+  inputRef.value.focus()
 }
- 
-async function addCouponQueue(coupon_data){
+
+async function addCouponQueue(coupon_data) {
   setTimeout(async () => {
-     delete coupon_data.coupon_status ;
+    delete coupon_data.coupon_status;
     const res = await app.createDoc("Coupon Codes", coupon_data);
-      if(res.data){
-            docListRef.value.changeStatus(coupon_data.id, {coupon_status:"Done",name:res.data.name})
-      }else {
-        const messages =  await handleErrorMessage(res.error)
-         docListRef.value.changeStatus(coupon_data.id,{ coupon_status:"Fail", message:(messages||[]).join(", ")})
-      }
+    if (res.data) {
+      docListRef.value.changeStatus(coupon_data.id, { coupon_status: "Done", name: res.data.name })
+    } else {
+      const messages = await handleErrorMessage(res.error)
+      docListRef.value.changeStatus(coupon_data.id, { coupon_status: "Fail", message: (messages || []).join(", ") })
+    }
   }, 500);
- 
+
 }
 
 function onRemoveCoupon() {
-    const couponNumber = app.utils.getCouponNumber(coupon?.value);
-    // find coupon index in coupon list
+  const couponNumber = app.utils.getCouponNumber(coupon?.value);
+  // find coupon index in coupon list
 
-    const index = coupounList.value.findIndex(x => x.coupon == coupon.value);
-    if (index == -1) {
-        coupon.value = "";
-        app.showWarning(t("coupon_code_number_does_not_exist", { coupon: couponNumber }))
-    } else {
-        onDelete(index);
+  const index = coupounList.value.findIndex(x => x.coupon == coupon.value);
+  if (index == -1) {
+    coupon.value = "";
+    app.showWarning(t("coupon_code_number_does_not_exist", { coupon: couponNumber }))
+  } else {
+    onDelete(index);
 
-    }
+  }
 }
- 
+
 onMounted(async () => {
   const name = app.route.params.name;
   const result = await app.getDoc('Coupon Register', name);
-  options.filters =[["coupon_register","=",result.data.name]]
+  options.filters = [["coupon_register", "=", result.data.name]]
   doc.value = result.data;
 });
 </script>
--- src/views/components/document-list/DocList.vue
+++ src/views/components/document-list/DocList.vue
@@ -4,118 +4,110 @@
     </ion-refresher>
 
     <slot name="searchBar">
-        <ComSearchBar v-if="options.showSearchBar" @onSearch="onSearch"
-        :focus="focus"
-            :showBarcodeScanner="options.showBarcodeScanner"  ref="txtSearchRef"/>
+        <ComSearchBar v-if="options.showSearchBar" @onSearch="onSearch" :focus="focus"
+            :showBarcodeScanner="options.showBarcodeScanner" ref="txtSearchRef" />
     </slot>
 
     <Loading v-if="loading" />
     <template v-else>
-        
+
         <slot name="filter">
-            <ion-grid class="filter-grid" >
+            <ion-grid class="filter-grid">
                 <ion-row>
-                    <ion-col :size="12" :size-md="10" >
-                        <ComFilter  :filterOptions="options.filterOptions" @onFilter="onFilter"/>
+                    <ion-col :size="12" :size-md="10">
+                        <ComFilter :filterOptions="options.filterOptions" @onFilter="onFilter" />
                     </ion-col>
-                    <ion-col :size="2" v-if="data.length>0" class="ion-hide-sm-down">
+                    <ion-col :size="2" v-if="data.length > 0" class="ion-hide-sm-down">
                         <div style="display: flex; justify-content: right;">
-                            <ion-chip  v-if="hideTopShowingRecord">
-                               {{ t("Showing Record") }} {{ data.length  }} {{ t("of") }} {{ totalRecord }}
+                            <ion-chip v-if="hideTopShowingRecord">
+                                {{ t("Showing Record") }} {{ data.length }} {{ t("of") }} {{ totalRecord }}
                             </ion-chip>
-                            <ion-button @click="onReloadData" fill="clear" shape="round"  v-tooltip.left="`${t('Refresh Data')}`">
-                                <ion-icon :icon="refreshOutline" slot="icon-only"/>
+                            <ion-button @click="onReloadData" fill="clear" shape="round"
+                                v-tooltip.left="`${t('Refresh Data')}`">
+                                <ion-icon :icon="refreshOutline" slot="icon-only" />
                             </ion-button>
-                            
+
                         </div>
                     </ion-col>
-                    
+
                 </ion-row>
             </ion-grid>
-            
+
         </slot>
 
         <slot v-if="data && data.length > 0" :item="data">
-            <template  v-if="plateform=='desktop'">
-            <DataTable :value="data" 
-                class="ion-padding-start ion-padding-end"
-                tableStyle="min-width: 50rem" selectionMode="single"
-                v-model:selection="selectedRow" 
-                showGridlines 
-                stripedRows 
-                @sort="onSort" 
-                :lazy="true"
-                @row-dblclick="onRowDblClick" 
-                :sortField="options.presort" 
-                :sortOrder="options.sortOrder || 1"
-            >
-                 <Column :header="t('No.')" headerClass="text-center" bodyClass="text-center" style="width: 60px">
-                    <template #body="slotProps">
-                        {{ slotProps.index + 1 }}
-                    </template>
-                </Column>
-                <Column v-for="col of options.columns" :key="col.fieldname" :field="col.fieldname"
-                    :header="t(col.header)" :headerClass="col.align || getAligment(col.fieldtype)"
-                    :bodyClass="col.align || getAligment(col.fieldtype)" sortable>
-                    
-                    <template #body="slotProps">
-                        <slot :name="col.fieldname" :item="slotProps.data" :index="col.fieldname + '_' + index">
-                            <!-- date column -->
-                            <template v-if="col.fieldtype == 'Date'">
-                                <template v-if="slotProps.data[col.fieldname]">
-                                         {{ dayjs(slotProps.data[col.fieldname]).format("DD-MM-YYYY") }}
+            <template v-if="plateform == 'desktop'">
+                <DataTable :value="data" class="ion-padding-start ion-padding-end" tableStyle="min-width: 50rem"
+                    selectionMode="single" v-model:selection="selectedRow" showGridlines stripedRows @sort="onSort"
+                    :lazy="true" @row-dblclick="onRowDblClick" :sortField="options.presort"
+                    :sortOrder="options.sortOrder || 1">
+                    <Column :header="t('No.')" headerClass="text-center" bodyClass="text-center" style="width: 60px">
+                        <template #body="slotProps">
+                            {{ slotProps.index + 1 }}
+                        </template>
+                    </Column>
+                    <Column v-for="col of options.columns" :key="col.fieldname" :field="col.fieldname"
+                        :header="t(col.header)" :headerClass="col.align || getAligment(col.fieldtype)"
+                        :bodyClass="col.align || getAligment(col.fieldtype)" sortable>
+
+                        <template #body="slotProps">
+                            <slot :name="col.fieldname" :item="slotProps.data" :index="col.fieldname + '_' + index">
+                                <!-- date column -->
+                                <template v-if="col.fieldtype == 'Date'">
+                                    <template v-if="slotProps.data[col.fieldname]">
+                                        {{ dayjs(slotProps.data[col.fieldname]).format("DD-MM-YYYY") }}
+                                    </template>
+
                                 </template>
-                                
-                            </template>
-                            <template v-else-if="col.fieldtype == 'Datetime'">
-                                <span v-tooltip="`${dayjs(slotProps.data[col.fieldname])}`">
-                                    {{ dayjs(slotProps.data[col.fieldname]).fromNow() }}
-                                </span>
-                            </template>
-                            <template v-else-if="col.fieldtype == 'Currency'">
-                                <ComCurrency :value="slotProps.data[col.fieldname]" />
-                            </template>
-                            <template v-else-if="col.url ">
+                                <template v-else-if="col.fieldtype == 'Datetime'">
+                                    <span v-tooltip="`${dayjs(slotProps.data[col.fieldname])}`">
+                                        {{ dayjs(slotProps.data[col.fieldname]).fromNow() }}
+                                    </span>
+                                </template>
+                                <template v-else-if="col.fieldtype == 'Currency'">
+                                    <ComCurrency :value="slotProps.data[col.fieldname]" />
+                                </template>
+                                <template v-else-if="col.url">
                                     <template v-if="col.id_field">
-                                         <router-link 
-                                                v-if="slotProps.data[col.id_field]"
-                                                :to="`${col.url}/${slotProps.data[col.id_field]}`">
-                                                {{ slotProps.data[col.fieldname] }}
-                                            </router-link>
-                                            <ion-label v-else>{{ slotProps.data[col.fieldname] }}</ion-label>
+                                        <router-link v-if="slotProps.data[col.id_field]"
+                                            :to="`${col.url}/${slotProps.data[col.id_field]}`">
+                                            {{ slotProps.data[col.fieldname] }}
+                                        </router-link>
+                                        <ion-label v-else>{{ slotProps.data[col.fieldname] }}</ion-label>
                                     </template>
                                     <template v-else>
-<router-link 
-                                    v-if="slotProps.data[col.fieldname]"
-                                    :to="`${col.url}/${slotProps.data[col.fieldname]}`">
-                                    {{ slotProps.data[col.fieldname] }}
-                                    </router-link>
-                                <ion-label v-else>{{ slotProps.data[col.fieldname] }}</ion-label>
+                                        <router-link v-if="slotProps.data[col.fieldname]"
+                                            :to="`${col.url}/${slotProps.data[col.fieldname]}`">
+                                            {{ slotProps.data[col.fieldname] }}
+                                        </router-link>
+                                        <ion-label v-else>{{ slotProps.data[col.fieldname] }}</ion-label>
                                     </template>
-                                    
-                            </template>
-                            <template v-else>
-                                <span v-if="col.fieldname == 'owner' || col.fieldname == 'modified_by'">
-                                    {{ slotProps.data[col.fieldname].split("@")[0] }}
-                                </span>
-                                <span v-else>
-                                    {{ slotProps.data[col.fieldname] }}
-                                </span>
-                                <!-- {{ getAligment(col.fieldtype) }} -->
-                            </template>
-                        </slot>
-                    </template>
-                </Column>
-            </DataTable>
-       </template>
-       <ComDocCard v-else :data="d" v-for="(d,index ) in data" :key="index" :fields="options.columns" 
-       :titleFields="options.title_fields ||  meta.title_field" :subTitleFields="options.subTitleFields || meta.search_fields"/>
+
+                                </template>
+                                <template v-else>
+                                    <span v-if="col.fieldname == 'owner' || col.fieldname == 'modified_by'">
+                                        {{ slotProps.data[col.fieldname].split("@")[0] }}
+                                    </span>
+                                    <span v-else>
+                                        {{ slotProps.data[col.fieldname] }}
+                                    </span>
+                                    <!-- {{ getAligment(col.fieldtype) }} -->
+                                </template>
+                            </slot>
+                        </template>
+                    </Column>
+                </DataTable>
+            </template>
+            <ComDocCard v-else :data="d" v-for="(d, index) in data" :key="index" :fields="options.columns"
+                :titleFields="options.title_fields || meta.title_field"
+                :subTitleFields="options.subTitleFields || meta.search_fields" />
 
         </slot>
         <div v-else>
             <slot name="empty">
-                
-                 <ComNoRecord :title="t('No Record Found')" :message="emptyRecordMessage" :actions="options.noRecordActions"/>
+
+                <ComNoRecord :title="t('No Record Found')" :message="emptyRecordMessage"
+                    :actions="options.noRecordActions" />
             </slot>
         </div>
         <div style="padding-bottom: 50px;" v-if="!options.disableLoadMore">
@@ -124,16 +116,17 @@
             </ion-infinite-scroll>
         </div>
     </template>
-<ion-chip v-if="data.length>0"  style="position: fixed; background: #e4eaf5; bottom: 16px; left: 50%; transform: translateX(-50%); z-index: 1000;">
-  {{ t("Showing Record") }} {{ data.length }} {{ t("of") }} {{ totalRecord }}
-</ion-chip>
+    <ion-chip v-if="data.length > 0"
+        style="position: fixed; background: #e4eaf5; bottom: 16px; left: 50%; transform: translateX(-50%); z-index: 1000;">
+        {{ t("Showing Record") }} {{ data.length }} {{ t("of") }} {{ totalRecord }}
+    </ion-chip>
 </template>
 <script setup>
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import dayjs from 'dayjs';
 
-import {  useAttrs, watch  } from "vue"
+import { useAttrs, watch } from "vue"
 
 import { useDocList } from '@/hooks/useDocList';
 import ComSearchBar from '../ComSearchBar.vue';
@@ -142,8 +135,8 @@ import ComFilter from '@/views/components/document-list/ComFilter.vue';
 import ComNoRecord from '@/views/components/document-list/ComNoRecord.vue';
 import { refreshOutline } from 'ionicons/icons';
 
-const plateform= app.utils.getPlateform();
- 
+const plateform = app.utils.getPlateform();
+
 const attrs = useAttrs();
 const t = window.t;
 
@@ -157,25 +150,25 @@ const props = defineProps({
             showBarcodeScanner: false,
             fields: ["name"]
         },
-        noRecordActions:[],
-        hideTopShowingRecord:{
-            type:Boolean,
-            default:false
+        noRecordActions: [],
+        hideTopShowingRecord: {
+            type: Boolean,
+            default: false
         }
     },
-    contentRef:Object,
-    focus:{
-        type:Boolean,
-        default:true
+    contentRef: Object,
+    focus: {
+        type: Boolean,
+        default: true
     }
 
 })
 
- 
-const emptyRecordMessage = t("empty_record_message", {doctype: props.docType})
+
+const emptyRecordMessage = t("empty_record_message", { doctype: props.docType })
 
 const { data, onRefresh, onLoadMore, onSearch, loading, getAligment,
-    onSort, options,onFilter,totalRecord,meta,txtSearchRef
+    onSort, options, onFilter, totalRecord, meta, txtSearchRef
 } = useDocList(props)
 
 const selectedRow = defineModel("selectedRow")
@@ -188,12 +181,12 @@ watch(() => props.options, async (newVal, oldVal) => {
 
 
 const onRefreshData = async (event) => {
-  await onRefresh(event);
-  if (attrs.onOnRefresh) {
-    emit("onRefresh", event);
-  } else {
-    event.target.complete();
-  }
+    await onRefresh(event);
+    if (attrs.onOnRefresh) {
+        emit("onRefresh", event);
+    } else {
+        event.target.complete();
+    }
 };
 
 const onReloadData = async () => {
@@ -209,32 +202,31 @@ function onRowDblClick(event) {
     if (event.data) {
         emit("onRowDblClick", event.data);
     }
- 
+
 }
 
-function addRecord(doc){
+function addRecord(doc) {
     data.value.unshift(doc)
 }
- 
-function changeStatus(id,updatedData){
-    const row =  data.value.find(x=>x.id == id);
-    if(row){
-      Object.keys(updatedData).forEach(key => {
-        row[key] = updatedData[key]
-    });
+
+function changeStatus(id, updatedData) {
+    const row = data.value.find(x => x.id == id);
+    if (row) {
+        Object.keys(updatedData).forEach(key => {
+            row[key] = updatedData[key]
+        });
 
     }
 }
- 
+
 
 defineExpose({
-   onRefresh,
-   onReloadData,
-   addRecord,
-   changeStatus
+    onRefresh,
+    onReloadData,
+    addRecord,
+    changeStatus
 })
 
- 
+
 
 </script>
- 
