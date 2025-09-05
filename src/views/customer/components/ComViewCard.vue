3cb6da28f26682447291ae1d5bcb5c42f06cd3eb
Sabrey <limsabriey94@gmail.com>
2025-09-05 14:06:32 +0700

    update

--- src/views/customer/components/ComViewCard.vue
+++ src/views/customer/components/ComViewCard.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="ion-padding">
   <stack row equal itemClass="col-6 sm:col-6 lg:col-4 " gap="0px">
-    <card color="primary" :value = "data?.total_visit ?? 0" label="Total Visit" />
-    <card color="warning" :value = "data?.total_annual_order ?? 0" label="Total Anual Order" />
-    <card color="success" :value = "data?.total_order ?? 0" label="Total Order" />
+    <card color="primary" :value="data?.total_visit ?? 0" label="Total Visit" :isCurrency="false" />
+<card color="warning" :value="data?.total_annual_order ?? 0" label="Total Annual Order" />
+<card color="success" :value="data?.total_order ?? 0" label="Total Order" />
+
   </stack>
   </div>
-   <!-- {{ data }} -->
+ 
 </template>
 
-<!-- <script setup>
-import { ref,  } from 'vue'
-const t = window.t
-const data = ref({"total_visit":0,"total_annual_order":0,"total_order":0})
-</script> -->
+ 
 
 <script setup>
 import { ref, onMounted, defineComponent, h } from 'vue'
@@ -27,9 +24,10 @@ const card = defineComponent({
   props: {
     label: String,
     color: String,
-    value: Number
+    value: Number,
+    isCurrency: { type: Boolean, default: true }
   },
-  setup(props) {
+    setup(props) {
     return () =>
       h('ion-card',
         { button: true, color: props.color, class: 'ion-no-margin' },
@@ -37,7 +35,9 @@ const card = defineComponent({
           h('ion-card-content', { class: 'text-center' }, [
             h('ion-card-subtitle',
               { slot: 'end', style: 'font-size: 18px;' },
-              [h(ComCurrency, { value: props.value })]
+              props.isCurrency
+                ? [h(ComCurrency, { value: props.value })]
+                : props.value?.toString()  
             ),
             h('ion-card-subtitle',
               { class: 'mt-2', style: 'font-size: 18px;' },
